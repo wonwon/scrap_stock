a876d323a5b2e151327e83056c6b84622fb8d0a6
wonwon <cool2ikou@gmail.com>
2020-06-08 00:13:37 +0900

    edit ADR scrap

---
+++ scrap/scrap_adr.py
@@ -0,0 +1,20 @@
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+from DB import class_sqlite
+
+base_uri = 'https://adr-stock.com/'
+
+def uri2soup(uri):
+    html = requests.get(uri)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    return soup
+
+def extractlink(soup):
+    data = soup.select('div#stbl')
+    return data
+
+soup = uri2soup(base_uri)
+print(extractlink(soup))
