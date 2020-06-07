21f48935ca86b9120039765aab8af86cd4e3158b
wonwon <cool2ikou@gmail.com>
2020-06-07 11:18:18 +0900

    edit config parser & changed configini

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import configparser
 
 config_ini = configparser.ConfigParser()
-config_ini.read('./CONF/config.ini', encoding = 'utf-8')
+config_ini.read('./../../CONF/config.ini', encoding = 'utf-8')
 
 #mail config
 config = {}
 
 config['FROM'] = config_ini['GMAIL']['USER']
 config['TO'] = config_ini['GMAIL']['USER']
-config['PASS'] =  config_ini['GMAIL']['USER']['PASS']
+config['PASS'] =  config_ini['GMAIL']['PASS']
+
+print(config)
