551b3a82d4b70d4ff48bc87375f0c57c3edb1ee4
wonwon <cool2ikou@gmail.com>
2020-06-07 11:59:15 +0900

    edit jinja2 template

--- make_body.py
+++ make_body.py
@@ -1,5 +1,11 @@
 from jinja2 import Environment, FileSystemLoader
 
+
+
 #gmail template
 env = Environment(loader = FileSystemLoader('./', encoding = 'utf8'))
 tmp = env.get_template('./tmp/gmail_html.tmpl')
+
+
+html = tmp.render({ 'mailbody': 'メール本文' })
+print(html)
