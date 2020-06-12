11bff494f49d3768551fc1bfdd13563d05441e37
wonwon <cool2ikou@gmail.com>
2020-06-13 01:38:03 +0900

    change sendmail name param

--- Gmail.py
+++ Gmail.py
@@ -13,16 +13,16 @@ class SendByGmail:
         self.__pass = config['PASS']
         self.__charset = 'utf-8'
 
-    def send(self, msg):
+    def send_gmail(self, msg):
         smtp_obj = smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout = 10)
         smtp_obj.login(self.__fromadd, self.__pass)
         smtp_obj.sendmail(self.__fromadd, self.__toadd, msg.as_string())
         smtp_obj.close()
 
-    def creat_message(self, content):
-        msg = MIMEText(content['body'], "html", self.__charset)
+    def creat_message(self, subject, content, types = 'plain'):
+        msg = MIMEText(content['body'], types, self.__charset)
         #msg.add_header('Content-type', 'text/html')
-        msg["Subject"] = content['body']
+        msg["Subject"] = subject
         msg["From"] = self.__fromadd
         msg["To"] = self.__toadd
         msg["Bcc"] = self.__tobcc
