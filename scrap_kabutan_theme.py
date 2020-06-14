9202096be1407bbdc757a25038b37274f12f5d27
wonwon <cool2ikou@gmail.com>
2020-06-14 20:21:58 +0900

    added tml

--- scrap_kabutan_theme.py
+++ scrap_kabutan_theme.py
@@ -12,6 +12,7 @@ import urllib.parse
 base_uri = 'https://kabutan.jp'
 rank_uri = '/info/accessranking/3_2'
 theme_uri = '/themes/?theme='
+stock_uri = '/stock/kabuka?code='
 page_uri = '&market=0&capitalization=-1&stc=zenhiritsu&stm=1&page='
 # kabureal uri
 kabureal = 'http://kabureal.net/brand/?code='
@@ -33,7 +34,10 @@ def extstockuri(uri):
     links = soup.select('td.tac > a')
     stocks = soup.select('table.stock_table > tr > td:nth-child(6)')
     for i in range(len(links)):
-        data[links[i].text] = stocks[i].text
+        if stocks[i].text == '－':
+            data[links[i].text] = '600'
+        else:
+            data[links[i].text] = stocks[i].text
 
     return data
 
@@ -43,52 +47,53 @@ def extkaburealdata(uri):
 
 def extstock(uri):
     soup = uri2soup(uri)
-    body_text = []
-    #stock price
-    price = soup.select('span.kabuka')
-    sp = re.match('([0-9,]+)', price[0].text).group(1).replace(',', '')
-    if int(sp) < 500:
-        #stock name category
-        cp = soup.select('h2')
-        ind = soup.select('#stockinfo_i2 > div')
-        # stock overview
-        sti = soup.select('#stockinfo_i3')[0].text.replace('\n', '')
-        # stock history
-        tbhead = soup.select('table.stock_kabuka0 > thead > tr')[0].text.replace('\n', ' ')
-        tbl1 = soup.select('table.stock_kabuka0 > tbody > tr')[0].text.replace('\n', ' ')
-        body_text.append(\
-            cp[0].text + '(' + ind[0].text.replace('\n', '') + ')' + '\n'\
-            + sti + '\n'\
-            + tbhead + '\n'\
-            + tbl1 + '\n')
-        # stock history table row
-        for i in range(5):
-            tbl2 = soup.select('table.stock_kabuka1 > tbody > tr:nth-child(-n+5)')[i].text.replace('\n', ' ')
-            body_text.append(tbl2 + '\n')
+    body = {}
+    #stock name category
+    body['name'] = soup.select('h2')[0].text
+    body['industory'] = soup.select('#stockinfo_i2 > div > a')[0].text.replace('\n', '')
+    # stock overview
+    body['info'] = soup.select('#stockinfo_i3 > table > tbody > tr:first-child')[0].text.replace('\n', '')
+    # stock history
+    body['tbhead'] = soup.select('table.stock_kabuka0 > thead > tr')[0].text.replace('\n', ' ')
+    body['past'] = soup.select('table.stock_kabuka0 > tbody > tr')[0].text.replace('\n', ' ')
+    # stock history table row
+    for i in range(5):
+        body['past' + str(i)] = soup.select('table.stock_kabuka1 > tbody > tr:nth-child(-n+5)')[i].text.replace('\n', ' ')
+         
+    return body
 
-    return body_text
+#gmail template
+env = Environment(loader = FileSystemLoader('./', encoding = 'utf8'))
+tmp = env.get_template('./tmp/gmail_html.tmpl')
 
 # extract themelink
 links = extlink(base_uri +  rank_uri)
 for i in range(5):
+    body = []
     # extract stock link & price links[i].text
     for j in range(1, 5):
-        time.sleep(2)
         list_uri = base_uri + theme_uri + urllib.parse.quote(links[i].text) + page_uri + str(j)
         data = extstockuri(list_uri)
         for code, stock in data.items():
             #extract under 600 yen
             if int(re.sub('\D', '', stock)) < 600:
-                print("Hit" + code + '-' + stock + 'yen')
+                time.sleep(2)
+                #extract kabureal img
                 img = extkaburealdata(kabureal + str(code))
-                print(img)
-            
+                inf = extstock(base_uri + stock_uri + str(code))
+                print(inf['name'])
+                body.append({
+                    'code' : str(code),
+                    'stock' : str(stock),
+                    'img' : img,
+                    'info' : inf['info'],
+                    'head' : inf['tbhead'],
+                    'table' : inf['past'],
+                })
 
+    print(links[i].text)
+    html = tmp.render({
+        'theme' : links[i].text,
+        'articles' : body })
+    print(html)
 
-#gmail template
-env = Environment(loader = FileSystemLoader('./', encoding = 'utf8'))
-tmp = env.get_template('./tmp/gmail_html.tmpl')
-#html = tmp.render({\
-#        'mailbody': stocklist['body'],\
-#         })
-#print(html)
---
+++ test.py
@@ -0,0 +1,7 @@
+a = '-'
+
+if a.isnumeric() :
+    print('ture')
+else:
+    print(type(a))
+    print('False')
